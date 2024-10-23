06df7799b23f32f3748ee721b0aeef8d96b76b59
Dylpickle2008 <dylanrigby1016@gmail.com>
2024-10-23 12:31:22 +0000

    init commit

---
+++ mad-libs.py
@@ -0,0 +1,29 @@
+activity1 = input("Type an activity")
+verb1 = input("Type a verb")
+noun1 = input("Type a proper noun")
+noun2 = input("Type a second proper noun")
+location1 = input("Type a location")
+dialogue1 = input("Type some brief dialogue")
+noun3 = input("Type a third noun")
+dialogue2 = input("Type a second segment  of dialogue")
+location2 = input("Type a second location")
+adjective1 = input("Type a # adjective")
+dialogue3 = input("Type a third segment of dialogue")
+dialogue4 = input("Type a fourth segment of dialogue")
+adjective2 = input("Type a second adjective")
+location3 = input("Type a third location")
+activity2 = input("Type a second activity")
+noun4 = input("Type a fourth noun")
+noun5 = input("Type a fifth proper noun")
+adjective3 = input("Type a third adjective")
+
+
+
+print (f"""Today, I woke up early in the morning and went {activity1} and {verb1}. After I drove to the location, I met up with {noun1} and {noun2} at the old {location1}. After we started walking and talked about {dialogue1}, 
+something unexpected occured. A big, scary {noun3} jumped out, revealing itself. We all screamed {dialogue2} and we ran in the other direction.
+After we all got tired, we hid in the {location2} and prayed that that horrid thing would pass. However, after {adjective1} minutes, we decided
+to take a peak. But we saw the {noun3} staring us dead in the eye, a face-distance away. We screamed in a delerious frenzy, tackling the {noun3}
+and overpwering it with ease. We tied it up, shouting {dialogue3} at it, before we tied the thing up and burned it. It shouted {dialogue4} loudly
+before burning up into ashes. We were all {adjective2} and let out a big sigh. I suggested that we go to the {location3} to {activity2} our close
+call. We all got in my friend's {noun1}'s {noun4} and drove off into the sunrise. A while into the drive, the thought came to me: Hey, where is {noun5}?
+A wave of shock it me as I remembered the creatures last words: {dialogue4}, as a sudden feeling of {adjective3}!""")
